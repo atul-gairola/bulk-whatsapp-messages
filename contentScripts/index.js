0234a492b92bd1df966ad62ca895ac2ebef37041
atul-gairola <atulgairola08@gmail.com>
2021-01-15 18:16:15 +0530

    Send message through the popup

---
+++ contentScripts/index.js
@@ -0,0 +1,54 @@
+// jquery setup
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const $id = document.getElementById.bind(document);
+
+// HELPER FUNCTION - delays the execution by given miliseconds
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// listens for the message from popup
+chrome.runtime.onMessage.addListener(async (message, sender, response) => {
+  if (message.from === "home_popup" && message.subject === "send_message") {
+    console.log(message.message);
+    await sendMessage(message.message);
+    await clickSendButton();
+  }
+});
+
+async function sendMessage(msg) {
+  return new Promise((resolve, reject) => {
+    const bulkWhatsappLink = $id("blkwhattsapplink");
+    if (bulkWhatsappLink) {
+      bulkWhatsappLink.setAttribute(
+        "href",
+        `https://wa.me/919958472522?text=${msg}`
+      );
+    } else {
+      const spanHtml = `<a href="https://wa.me/919958472522?text=${msg}" id= "blkwhattsapplink"></a>`;
+      const spans = $$("#app .app-wrapper-web span");
+      spans[4].innerHTML = spanHtml;
+    }
+
+    setTimeout(() => {
+      document.getElementById("blkwhattsapplink").click();
+      resolve();
+    }, 1000);
+  });
+}
+
+async function clickSendButton() {
+  await waitFor("[data-icon=send]");
+  if ($("[data-icon=send]")) {
+    $("[data-icon=send]").click();
+  }
+}
+
+// helper function keeps waiting recursively for the given element query until found on the DOM
+async function waitFor(DOMQuery) {
+  if (!$(DOMQuery)) {
+    setTimeout(async function () {
+      await waitFor(DOMQuery);
+    }, 500);
+  }
+  return;
+}
--- pages/home/popup.js
+++ pages/home/popup.js
@@ -5,3 +5,46 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     window.location.href = "../incorrectActiveTab/popup.html";
   }
 });
+
+// mimicking jquery syntax
+$id = document.getElementById.bind(document);
+
+// DOM elements
+const numbers_input = $id("numbers");
+const message_input = $id("message");
+const submit_button = $id("submit");
+
+// state for form data
+const form_data = {
+  numbers: "",
+  message: "",
+};
+
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  form_data[name] = value;
+  console.log(form_data);
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        from: "home_popup",
+        subject: "send_message",
+        message: form_data.message,
+      });
+    }
+  );
+};
+
+numbers_input.addEventListener("change", handleChange);
+
+message_input.addEventListener("change", handleChange);
+
+submit_button.addEventListener("click", handleSubmit);
